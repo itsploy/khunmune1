6a70debf86d6d0c2193852e7667315d94dd63ba5
itsploy <115002439+itsploy@users.noreply.github.com>
2023-07-02 14:35:46 +0700

    Add files via upload

---
+++ index_member.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> ร้านก๋วยเตี๋ยวเรือคุณหมื่น สูตรโบราณ</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#E55002">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#E55002">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#E55002">
+		
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+	
+<style>
+@font-face {
+  font-family: Kanit-Light;
+  src: url('fonts/Kanit-Light.ttf'); 
+}
+ 
+.serif {
+  font-family:  Kanit-Light, serif;
+}
+
+</style>
+	
+<body  class="serif"   style=" background-image: url('images/bg2.png');
+  background-repeat: no-repeat; 
+  background-attachment: fixed;
+  background-size: cover;" >
+	 
+	
+	<div class=" " style="margin-top: -50px;">
+		<div class="container-login100"  >
+			<div  >
+				<form  action="check_login2.php" method="post" >
+					
+					<div align="center"  >  
+					 <img src="logo.png" style="height: 150px; margin-bottom: 10px; " > 
+				    </div>
+					
+					<div align="center" style="margin-top: 20px; border:  0px solid #000;"   >  
+					<font size="4px" color="#000000"> 
+						<font size="4px" color="#000000">   ร้านก๋วยเตี๋ยวเรือคุณหมื่น <br> </font>
+						<font size="4px" color="#000000"> สูตรโบราณ <br> </font> 
+					</font>
+				    </div>
+					   
+					<div class="col-lg-12" style="margin-top: 40px; border:  0px solid #000;" >
+			    	
+			    	 <a href="register/index.php">   
+				     <button type="button" class="btn btn-sm "  style="background-color: #CF6404; border: 1px solid #CF6404;  border-radius: 20px; margin-top: 5px; font-size: 10px; width: 100%; height: 32px;  "> 
+					 <font size="2px" color="white" >  จองโต๊ะที่นั่ง  </font> </button>   </a>
+			    	
+			    	
+			    	<a href="ca/index.php">
+			    		 
+				     <button type="button"  class="btn btn-sm "  style=" background-color: #FFFFFF; border: 1px solid #CF6404;  border-radius: 20px; margin-top: 15px; font-size: 10px;  width: 100%;  height: 32px;   "   />   
+					 <font size="2px" color="#000" >   เเสกนคิวอาร์โค้ดสั่งอาหาร   </font> </button>
+			    	
+			    	</a>
+				    	   
+				
+				
+					</div>
+   
+				</form>
+			</div>
+		</div>
+	</div>
+	
+ 
+				 
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+	 
+</body>
+</html>
